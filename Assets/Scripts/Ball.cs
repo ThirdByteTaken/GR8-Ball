6887161ebbf73ecede7bef798e21e09231c8f8ad
Andrew Hartnell <62224502+ThirdByteTaken@users.noreply.github.com>
2022-04-29 08:22:26 -0700

    Add basic win/lose system

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,10 +7,24 @@ public class Ball : MonoBehaviour
     Animator animator;
     Rigidbody rb_Ball; // It doens't like us naming it rigidbody
     AudioSource audioSource;
+
     const float BallSpeed = 10;
+
+    [SerializeField]
+    bool IsEightBall;
+
+    [SerializeField]
+    bool BallIsStriped;
+
+    private static int StripedBalls;
+    private static int SolidBalls;
+    private static bool IsPlayingStripes;
+
     // Start is called before the first frame update
     void Start()
     {
+        StripedBalls = 7;
+        SolidBalls = 7;
         animator = GetComponent<Animator>();
         rb_Ball = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
@@ -28,6 +42,21 @@ public class Ball : MonoBehaviour
         if (collider.gameObject.CompareTag("Hole"))
         {
             collider.GetComponent<AudioSource>().Play();
+            if (StripedBalls == 7 && SolidBalls == 7) IsPlayingStripes = BallIsStriped;
+
+            if (!IsEightBall)
+            {
+                if (BallIsStriped) StripedBalls--;
+                else SolidBalls--;
+            }
+            else
+            {
+                if ((IsPlayingStripes && StripedBalls == 0) || (!IsPlayingStripes && SolidBalls == 0))
+                    print("Winner");
+                else
+                    print("Game Over");
+            }
+
             Destroy(rb_Ball);
             animator.SetTrigger("GameOver");
         }

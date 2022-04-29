51d1d27c970edb1b9103505eb092e7ed294ca7c5
Andrew Hartnell <62224502+ThirdByteTaken@users.noreply.github.com>
2022-04-29 07:32:06 -0700

    Add basic music/sound

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,12 +6,14 @@ public class Ball : MonoBehaviour
 {
     Animator animator;
     Rigidbody rb_Ball; // It doens't like us naming it rigidbody
+    AudioSource audioSource;
     const float BallSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rb_Ball = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,8 +27,17 @@ public class Ball : MonoBehaviour
         print("hit");
         if (collider.gameObject.CompareTag("Hole"))
         {
+            collider.GetComponent<AudioSource>().Play();
             Destroy(rb_Ball);
             animator.SetTrigger("GameOver");
         }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ball") && Random.Range(0, 1f) > 0.5) return;
+        audioSource.pitch = Random.Range(0.5f, 0.8f);
+        audioSource.volume = Random.Range(0.5f, 0.8f);
+        audioSource.Play();
     }
 }
